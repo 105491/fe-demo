80d4feec4611541ce16ff5be42178dc01b840b14
wangyuanlu <644782210@qq.com>
2023-05-17 14:16:16 +0800

    杨健军的作业20230517

--- practice.ts
+++ practice.ts
@@ -17,6 +17,21 @@
   2. 函数要通过泛型支持不同类型的元组去重 push
   3. 要对实例中的元数据进行类型定义
 */
+interface AType<T,U> {
+  id:T;
+  uname:U;
+}
+type _Arr1 = Array<AType<number,string>>
+const a:_Arr1 = [{id: 1, uname: '123'}, {id: 2, uname: '1234'}, {id: 3, uname: '1234'}]
+function uniquePush(sourceItem: AType<number,string>, targetArray: Array<AType<number,string>>, ident: string) {
+  let targetIndex = targetArray.findIndex(obj => obj[ident] === sourceItem[ident]);
+  if (targetIndex === -1) {
+    targetIndex = targetArray.length;
+  }
+
+  targetArray.splice(targetIndex, 1, sourceItem);
+};
+uniquePush({id: 1, uname: 'dddd'}, a, 'id');
 export function demo (){
   console.log('test start')
 }
