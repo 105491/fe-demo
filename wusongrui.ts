8d3f24c8d7b3f23562560559e7737254520b85aa
wangyuanlu <644782210@qq.com>
2023-05-18 14:15:03 +0800

    吴崧睿的作业20230518

---
+++ wusongrui.ts
@@ -0,0 +1,28 @@
+
+
+interface aInterface {id:number;uname:string}
+
+type aType =aInterface[];
+var a:aType = [{ id: 1, uname: '123' }, { id: 2, uname: '1234' }, { id: 3, uname: '1234' }]
+
+
+export function uniquePush<U extends {}>(sourceItem:U, targetArray:U[], ident:keyof U):void{
+  let targetIndex:number = targetArray.findIndex(obj => obj[ident] === sourceItem[ident]);
+  if (targetIndex === -1) {
+    targetIndex = targetArray.length;
+  }
+
+  targetArray.splice(targetIndex, 1, sourceItem);
+};
+
+uniquePush({ id: 1, uname: 'dddd' }, a, 'id');
+
+/* 
+  使用 ts 改写上述 js 代码
+  1. 完成后的函数支持相应的类型提示
+  2. 函数要通过泛型支持不同类型的元组去重 push
+  3. 要对实例中的元数据进行类型定义
+*/
+// export function demo() {
+//   console.log('test start')
+// }
