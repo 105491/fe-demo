84f9d2830237f5dce0a40e75348a516a4c01d071
dashi <dashi@dashiyun.cc>
2023-05-22 18:29:54 +0800

    zuoye

---
+++ practice2.ts
@@ -0,0 +1,26 @@
+// practice2.ts 作业 2
+// 
+// type Fish = {
+//   swim: () => void
+// }
+// type Dog = {
+//   bark: () => void
+// }
+// function isFish(pet: Fish | Dog): pet is Fish {
+//   return (pet as Fish).swim !== undefined;
+// }
+// const pet: Fish = {
+//   swim: () => true
+// }
+// function petAction(pet: Fish | Dog ){
+//   if (isFish(pet)) {
+//     pet.swim();
+//   } else {
+//     pet.bark();
+//   }
+// }
+
+// petAction(pet)
+/* 
+  1. 模仿上述例子，实现一个类型守卫函数
+*/
