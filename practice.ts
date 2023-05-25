da1eb7a6facc4c842f525b36a88baead87686c8d
zhyuelin <zhyuelin@163.com>
2023-05-25 22:23:56 +0800

    作业1

--- practice.ts
+++ practice.ts
@@ -1,12 +1,12 @@
 // var a = [{id: 1, uname: '123'}, {id: 2, uname: '1234'}, {id: 3, uname: '1234'}]
 
 // export function uniquePush(sourceItem, targetArray, ident) {
-//   let targetIndex = targetArray.findIndex(obj => obj[ident] === sourceItem[ident]);
-//   if (targetIndex === -1) {
-//     targetIndex = targetArray.length;
-//   }
+  // let targetIndex = targetArray.findIndex(obj => obj[ident] === sourceItem[ident]);
+  // if (targetIndex === -1) {
+  //   targetIndex = targetArray.length;
+  // }
 
-//   targetArray.splice(targetIndex, 1, sourceItem);
+  // targetArray.splice(targetIndex, 1, sourceItem);
 // };
 
 // uniquePush({id: 1, uname: 'dddd'}, a, 'id');
@@ -17,6 +17,22 @@
   2. 函数要通过泛型支持不同类型的元组去重 push
   3. 要对实例中的元数据进行类型定义
 */
-export function demo (){
-  console.log('test start')
+interface User {
+  id: number,
+  uname: string
 }
+
+type Arr = User[]
+
+const a: Arr = [{ id: 1, uname: '123' }, { id: 2, uname: '1234' }, { id: 3, uname: '1231' }]
+
+export function uniquePush<T1 extends User, T2 extends Arr>(sourceItem: T1, targetArray: T2, ident: string):void {
+  let targetIndex = targetArray.findIndex(obj => obj[ident] === sourceItem[ident]);
+  if (targetIndex === -1) {
+    targetIndex = targetArray.length;
+  }
+
+  targetArray.splice(targetIndex, 1, sourceItem);
+}
+
+uniquePush({ id: 1, uname: 'dddd' }, a, 'id');
