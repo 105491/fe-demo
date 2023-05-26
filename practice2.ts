83906edc7ea1734790fec2f1989380e2f04dba09
kluass <771618239@qq.com>
2023-05-26 09:32:07 +0800

    5-22作业

---
+++ practice2.ts
@@ -0,0 +1,57 @@
+// practice2.ts 作业 2
+
+// type Fish = {
+//   swim: () => void
+// }
+// type Dog = {
+//   bark: () => void
+// }
+// function isFish(pet: Fish | Dog): pet is Fish {
+//   return (pet as Fish).swim !== undefined;
+// }
+// const pet: Fish = {
+//   swim: () => true
+// }
+// function petAction(pet: Fish | Dog ){
+//   if (isFish(pet)) {
+//     pet.swim();
+//   } else {
+//     pet.bark();
+//   }
+// }
+
+// petAction(pet)
+/* 
+  1. 模仿上述例子，实现一个类型守卫函数
+*/
+
+
+/* 
+  2. 模仿例子实现一个类型提取，获取某个函数所有参数的类型
+*/
+// 例子：
+// type FirstType<T extends unknown[]> = T extends [infer a, ...unknown[]] ? a : never;
+// type first = FirstType<[1, 2]>;
+
+
+type chicken = {
+  chickenEatRice: () => void
+}
+type duck = {
+  duckEatRice: () => void
+}
+function isChicken(v: chicken | duck): v is chicken {
+  return (v as chicken).chickenEatRice !== undefined;
+}
+const v: chicken = {
+  chickenEatRice: () => true
+}
+function fun(v: chicken | duck ){
+  if (isChicken(v)) {
+    v.chickenEatRice();
+  } else {
+    v.duckEatRice();
+  }
+}
+
+fun(v)
