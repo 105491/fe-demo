2879effc2de7318e3a4b9601e8aadb6d28ef4b54
chenhaoliang <chenhaoliang@tuzhanai.com>
2023-05-26 15:07:46 +0800

    feat: 作业2

---
+++ zuoye2.ts
@@ -0,0 +1,54 @@
+// practice2.ts 作业 2
+
+// type Fish = {
+//   swim: () => void
+// }
+// type Dog = {
+//   bark: () => void
+// }
+// function isFish(pet: Fish | Dog): pet is Fish {
+//   return (pet as Fish).swim !== undefined;
+// }
+// const pet: Fish = {
+//   swim: () => true
+// }
+// function petAction(pet: Fish | Dog ){
+//   if (isFish(pet)) {
+//     pet.swim();
+//   } else {
+//     pet.bark();
+//   }
+// }
+
+// petAction(pet)
+/* 
+  1. 模仿上述例子，实现一个类型守卫函数
+*/
+
+type Cat = {
+  meow: () => void
+}
+type Bear = {
+  tweet: () => void
+}
+function isCatAnimal(animal: Cat | Bear): animal is Cat {
+  return (animal as Cat).meow !== undefined;
+}
+function animalAction(animal: Cat | Bear) {
+  if (isCatAnimal(animal)) {
+    animal.meow();
+  } else {
+    animal.tweet();
+  }
+}
+const cat: Cat = {
+  meow: () => console.log('meow')
+}
+animalAction(cat);
+
+/* 
+  2. 模仿例子实现一个类型提取，获取某个函数所有参数的类型
+*/
+// 例子：
+// type FirstType<T extends unknown[]> = T extends [infer a, ...unknown[]] ? a : never;
+// type first = FirstType<[1, 2]>;
